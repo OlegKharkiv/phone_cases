5b556d24edca209a7ab8c3c00a544b6deec7f6db
Oleg Kysil <olegkysil@olegs-mbp.highland.lan>
2024-05-26 23:18:58 +0300

    The issue postalCode handeled

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -57,10 +57,8 @@ const { mutate: createPaymentSession } = useMutation({
 
 const handleCheckout = () => {
     if(user) {
-//create payment session
         createPaymentSession({ configId: id })
     } else {
-        // need to log in
         localStorage.setItem('configurationId', id)
         setIsLoginModalOpen(true)
     }
@@ -81,7 +79,7 @@ const handleCheckout = () => {
             <div className='mt-20 grid grid-cols-1 text-sm sm:grid-cols-12 sm:grid-rows-1 sm:gap-x-6 md:gap-x-8 lg:gap-x-12'>
                 <div className='sm:col-span-4 md:col-span-3 md:row-span-2 md:row-end-2'>
                     <Phone
-                        className={cn(`bg-${tw}`)} 
+                        className={cn(`bg-${tw}`, 'max-w-[150px] md:max-w-full')} 
                         imgSrc={configuration.croppedImageUrl!} />
                 </div>
                 <div className='mt-6 sm:col-span-9 sm:mt-0 md:row-end-1'>
